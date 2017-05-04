a45e21879fd0d4447ce15d5c84950f651a4e5952
Bernardo Breder <bbreder@tecgraf.puc-rio.br>
2017-05-04 16:51:34 -0300

    .

---
+++ semana6/index.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict'
+    angular.module('app', []).controller('controller', ($scope) => {
+        $scope.name = 'Bernardo'
+        $scope.sayHello = () => 'Hello'
+    })
+})()
