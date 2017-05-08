4b92e9921f57498af5eed214d11a1d70c1536e5c
Bernardo Breder <bbreder@tecgraf.puc-rio.br>
2017-05-08 09:30:53 -0300

    .

---
+++ semana7/app.js
@@ -0,0 +1,27 @@
+
+angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController)
+    
+LunchCheckController.$inject = ['$scope']
+function LunchCheckController($scope) {
+
+	$scope.input = ""
+	$scope.msg = ""
+	
+    $scope.click = function() {
+		var input = $scope.input.trim().split(',').map(e=>e.trim()).filter(e=>e.length > 0)
+	
+		if (input.length == 0) {	
+			return $scope.msg = "Please enter data first"
+		}
+		
+		$scope.input = ""
+		
+		if (input.length <= 3) {
+			$scope.msg = 'Enjoy!'
+		} else {
+			$scope.msg = 'Too much!'
+		}
+	}
+
+}
